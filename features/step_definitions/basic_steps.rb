5946c136d8ddaf424b346eb05cfc660530e18e62
Aditya Naik <naik.aditya@gmail.com>
2018-11-21 12:05:56 +0100

    Adds tests to click on create button, all tests going green

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -7,7 +7,7 @@ Then("I should be able to fill in {string} with {string}") do |field, content|
     fill_in field, with: 'content'
 end
 
-Then("I should be able to click {string} button to submit form") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Then("I should be able to click {string} button to submit form") do |button|
+    click_on button
 end
   
