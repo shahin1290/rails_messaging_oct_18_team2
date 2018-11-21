f9ba2ff9919d864e2c5307d8a1796cb7c0480f2e
Aditya Naik <naik.aditya@gmail.com>
2018-11-21 11:31:34 +0100

    Creates basic_steps.rb and adds relevant functions

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,13 @@
+
+When("I visit the site") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be able to fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be able to click {string} button to submit form") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
