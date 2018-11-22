2436f4c67719c64cb0750a7d30ba18047ff48d40
Johanna Andén <johannaanden@Johannas-MacBook-Pro.local>
2018-11-22 14:53:08 +0100

    Compleats log in using Warden

--- features/step_definitions/compose_message_steps.rb
+++ features/step_definitions/compose_message_steps.rb
@@ -4,13 +4,10 @@ Given("following users exists") do |table|
     end
 end
 
-Given("a user exists") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Given("I am logged in as {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+Given(/^I am logged in as "([^"]*)"$/) do |name|
+  user = User.find_by(name: name)
+  login_as(user, scope: :user)
+end
 
 Given("I am on the {string} page") do |value|
     visit mailbox_inbox_path
