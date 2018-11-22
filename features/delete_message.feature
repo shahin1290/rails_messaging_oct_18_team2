65a0cdea7bed5ed1e8e55bbf613ffc41265da60f
shahin1290 <s.patowary@yahoo.com>
2018-11-22 13:42:57 +0100

    Adds delete_message feature and adds basic steps and sets up factory bot

---
+++ features/delete_message.feature
@@ -0,0 +1,23 @@
+Feature: Delete Message from inbox
+    As a user
+    In order to remove messages from my inbox
+    I want be able to delete and move my message to the trash bin
+
+Background:
+  Given following users exists
+    | name   | email             | password | password_confirmation |
+    | Jenny  | jenny@ranom.com   | password | password              |
+    | Daniel | daniel@random.com | password | password              |
+
+Scenario: Deleting a message
+  Given I am logged in as "Daniel"
+  And I send a mail to "Jenny"
+  And I am on the "home page"
+  And I click on the "Logout" link
+  Given I am logged-in as "Jenny"
+  And I am on the "home page"
+  And I click on the "Inbox" link
+  Then I should have "1" messages
+  And I click on the "View" link
+  And I click on the "Move to trash" link
+  Then I should have "0" messages
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -27,3 +27,40 @@ Then("Show me the page") do
     save_and_open_page
 end
 
+# Delete_Message_Feature
+
+Given("following users exists") do |table|
+    table.hashes.each do |user|
+        FactoryBot.create(:user, user)
+    end
+end
+  
+Given("I am logged in as {string}") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I send a mail to {string}") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the {string}") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click on the {string} link") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am logged-in as {string}") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should have {string} messages") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I click on the {string} link") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+  
+
